d843ca7195400c7904e760b0223fabb572a624ec
Azel F <92800306+AzelCH@users.noreply.github.com>
2022-04-03 07:48:38 +0700

    Create Main.php

---
+++ src/AzelCH/SimpleChatCensor/Main.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AzelCH\ChatCensor;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+
+use function str_replace;
+
+class Main extends PluginBase implements Listener {
+  
+  public function onEnable(): void{
+    $this->saveResource("config.yml");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onChat(PlayerChatEvent $e){
+    $p = $e->getPlayer();
+    $allcfg = $this->getConfig()->getAll();
+    $msg = str_replace($allcfg["badwords"], $this->getConfig()->get("replace-format"), $e->getMessage());
+    $e->setMessage($msg);
+
+  }
+  
+}
